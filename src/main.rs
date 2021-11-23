a4cf7b1843e356a610d5f925044c08c26741b2ea
Mauricio Muratori <maurimura@gmail.com>
2021-11-22 21:19:00 -0300

    Bot working locally with ping and echo

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::framework::standard::{
+    macros::{command, group},
+    CommandResult, StandardFramework,
+};
+use serenity::model::channel::Message;
+
+use std::env;
+
+use dotenv::dotenv;
+
+#[group]
+#[commands(ping, echo)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn echo(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, msg.content.clone()).await?;
+
+    Ok(())
+}
